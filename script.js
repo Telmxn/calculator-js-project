42f926f226cf6949ab472c0a509bc764063c6ccc
Telman Yusifov <43857129+Telmxn@users.noreply.github.com>
2024-02-17 22:28:14 +0400

    Added active elements.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const display = document.querySelector('#display');
-const numbers = document.querySelectorAll('.number');
-const operators = document.querySelectorAll('.operand');
-const clear = document.querySelector('#clear');
-const equal = document.querySelector('#equals');
-const decimal = document.querySelector('#decimal');
-const backspace = document.querySelector('#backspace');
+const display = document.querySelector("#display");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operand");
+const clear = document.querySelector("#clear");
+const equal = document.querySelector("#equals");
+const decimal = document.querySelector("#decimal");
+const backspace = document.querySelector("#backspace");
 
-let displayValue = '';
+let displayValue = "";
 let number1 = null;
 let number2 = null;
 let operator = null;
@@ -14,74 +14,76 @@ let hasCalculated = false;
 let typingNumber2 = false;
 
 numbers.forEach((numberInput) => {
-  numberInput.addEventListener('click', (e) => {
+  numberInput.addEventListener("click", (e) => {
     appendToDisplay(e.target.value);
   });
 });
-equal.addEventListener('click', (e) => {
+equal.addEventListener("click", (e) => {
   calculate();
 });
-clear.addEventListener('click', (e) => {
+clear.addEventListener("click", (e) => {
   clearDisplay();
   enableDecimalButton();
 });
-decimal.addEventListener('click', (e) => {
-  appendToDisplay('.');
+decimal.addEventListener("click", (e) => {
+  appendToDisplay(".");
   this.disabled = true;
 });
-backspace.addEventListener('click', (e) => {
+backspace.addEventListener("click", (e) => {
   backSpace();
 });
 
 window.onkeydown = function (e) {
-  if (e.key === 'Enter') {
+  if (e.key === "Enter") {
     calculate();
     return;
   }
-  if (e.key === 'Backspace') {
+  if (e.key === "Backspace") {
     backSpace();
     return;
   }
   if (
-    ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10'].includes(
+    [".", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"].includes(
       e.key
     )
   ) {
     appendToDisplay(e.key);
-  } else if (['-', '+', '*', '/'].includes(e.key)) {
+  } else if (["-", "+", "*", "/"].includes(e.key)) {
     operatorClicked(e.key);
   }
 };
 
 function operatorClicked(value) {
-    if (number1 === null) {
-      number1 = parseFloat(displayValue);
-      operator = value;
-      updateDisplay(operator); 
-    } else if (number1 !== null && operator !== null && typingNumber2) {
-      number2 = parseFloat(displayValue);
-      number1 = operate(number1, number2, operator);
-      display.value = number1;
-      number2 = null;
-      typingNumber2 = false;
-    }
-    operator = value;
-    decimal.disabled = false;
+  if (number1 === null) {
+    number1 = parseFloat(displayValue);
+  } else if (number1 !== null && operator !== null && typingNumber2) {
+    number2 = parseFloat(displayValue);
+    number1 = operate(number1, number2, operator);
+    display.value = number1;
+    number2 = null;
+    typingNumber2 = false;
   }
-  
-  function updateDisplay(symbol) {
-    display.value = display.value + ' ' + symbol + ' '; 
-  }
-  
+  operator = value;
+  document.querySelectorAll(".operand").forEach((operand) => {
+    operand.classList.remove("active");
+  });
+  document
+    .querySelector(`.operand[value='${operator}']`)
+    .classList.add("active");
+  decimal.disabled = false;
+}
+
+function updateDisplay(symbol) {
+  display.value = display.value + " " + symbol + " ";
+}
 
 operators.forEach((operatorInput) => {
-  operatorInput.addEventListener('click', (e) => {
-    if (e.target.value === '=') return;
+  operatorInput.addEventListener("click", (e) => {
+    if (e.target.value === "=") return;
 
     operatorClicked(e.target.value);
   });
 });
-
 
 function calculate() {
   if (number1 !== null && operator !== null) {
@@ -92,23 +94,23 @@ function calculate() {
       number1 = result;
       number2 = null;
       operator = null;
-      displayValue = '';
+      displayValue = "";
       typingNumber2 = false;
     } else {
-      number1 = null; 
+      number1 = null;
     }
   }
 }
 
 function appendToDisplay(value) {
   if (number1 !== null && !typingNumber2) {
-    display.value = '';
-    displayValue = '';
+    display.value = "";
+    displayValue = "";
     typingNumber2 = true;
   }
-  
-  if (value === '.' && displayValue.includes('.')) {
-    return; 
+
+  if (value === "." && displayValue.includes(".")) {
+    return;
   }
 
   const limit = 26;
@@ -122,8 +124,7 @@ function appendToDisplay(value) {
     }
   }
 
- 
-  if (value === '.') {
+  if (value === ".") {
     decimal.disabled = true;
   }
 }
@@ -132,21 +133,23 @@ function enableDecimalButton() {
   decimal.disabled = false;
 }
 
-
 function clearDisplay() {
-  display.value = '';
-  displayValue = '';
+  display.value = "";
+  displayValue = "";
   number1 = null;
   number2 = null;
   operator = null;
   typingNumber2 = false;
   enableDecimalButton();
+  document.querySelectorAll(".operand").forEach((operand) => {
+    operand.classList.remove("active");
+  });
 }
 
 function backSpace() {
   if (displayValue) {
     displayValue = displayValue.substring(0, displayValue.length - 1);
-    document.getElementById('display').value = displayValue;
+    document.getElementById("display").value = displayValue;
   }
 }
 
@@ -168,20 +171,23 @@ function divide(number1, number2) {
 }
 
 function operate(number1, number2, operator) {
+  document.querySelectorAll(".operand").forEach((operand) => {
+    operand.classList.remove("active");
+  });
   switch (operator) {
-    case '+':
+    case "+":
       return add(number1, number2);
-    case '-':
+    case "-":
       return subtract(number1, number2);
-    case '*':
+    case "*":
       return multiply(number1, number2);
-    case '/':
+    case "/":
       if (number2 === 0) {
         clearDisplay();
-        return 'Error! ';
+        return "Error! ";
       }
       return divide(number1, number2);
     default:
-      return number1; 
+      return number1;
   }
 }
